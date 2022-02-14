a9304b1a4693f497674fde120f4c926465bc280f
Felipe Reyes <felipe.reyes@canonical.com>
2022-02-13 23:30:31 -0300

    Install openidc-test-ipsilon snap.
    This change implements the support to install openidc-test-ipsilon from
    the store or from an attached resource.

--- src/charm.py
+++ src/charm.py
@@ -1,104 +1,99 @@
 #!/usr/bin/env python3
-# Copyright 2022 Felipe
-# See LICENSE file for licensing details.
+# Copyright 2022 Canonical Ltd
 #
-# Learn more at: https://juju.is/docs/sdk
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
-
+import hashlib
 import logging
+import os
 
+from charmhelpers.core import host
+from charmhelpers.fetch.snap import snap_install
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
+SNAP_NAME = 'openidc-test-ipsilon'
+SNAP_STORE = 'snap-store'
+IPSILON_DAEMON = 'snap.%s.daemon' % SNAP_NAME
+
 
 class OpenidcTestFixtureCharm(CharmBase):
-    """Charm the service."""
+    '''Charm the service.'''
 
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+        self.framework.observe(self.on.update_status, self._on_update_status)
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        self._stored.set_default(snap_hash=None)
+        self._stored.set_default(installation_source=None)
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+    def _on_install(self, _):
+        '''Install the workload.'''
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
-                }
-            },
-        }
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+        self._install_snap_if_needed()
+        self.unit.status = ActiveStatus('Installed.')
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+        '''config-changed handler.'''
+        self._install_snap_if_needed()
+        self._on_update_status(_)
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+    def _install_snap_if_needed(self):
+        '''Install or upgrade openidc-test-ipsilon if needed.'''
+        ipsilon_fpath = self.model.resources.fetch('openidc-test-ipsilon')
 
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        if ipsilon_fpath:
+            snap_hash = self._hash_resource(ipsilon_fpath)
+            if self._stored.snap_hash != snap_hash:
+                logger.debug('Using resource attached to install %s', SNAP_NAME)
+                snap_install(os.path.abspath(ipsilon_fpath),
+                             '--dangerous', '--classic')
+                self._stored.installation_method = 'resource'
+                self._stored.snap_hash = snap_hash
+            else:
+                logger.debug('Snap resource has not changed.')
+        elif self._stored.installation_method != SNAP_STORE:
+            channel = self.config["snap-channel"]
+            logger.debug('Installing %s from the snap store', SNAP_NAME)
+            snap_install(SNAP_NAME, '--classic',
+                         '--channel', channel)
+            self._stored.installation_method = SNAP_STORE
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
+    def _on_update_status(self, _):
+        if host.service_running(IPSILON_DAEMON):
+            status = ActiveStatus('%s running.' % IPSILON_DAEMON)
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            status = BlockedStatus('%s not running' % IPSILON_DAEMON)
+
+        self.unit.status = status
+
+    def _hash_resource(self, fpath: str) -> str:
+        sha256_hash = hashlib.sha256()
+        with open(fpath, 'rb') as f:
+            # Read and update hash string value in blocks of 4K
+            for byte_block in iter(lambda: f.read(4096), b''):
+                sha256_hash.update(byte_block)
+
+        return sha256_hash.hexdigest()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(OpenidcTestFixtureCharm)
